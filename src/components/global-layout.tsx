ecbf0941f55bb64ab750d67ecc6f9a946f857e1e
BAEK'space <112856532+100space@users.noreply.github.com>
2024-09-14 00:46:20 +0900

    Feat : 240913

---
+++ src/components/global-layout.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link"
+
+export default function GlobalLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <div>
+            <header>
+                <Link href={"/"}>📚 ONEBITE BOOKS</Link>
+            </header>
+            <main>{children}</main>
+            <footer>푸터</footer>
+        </div>
+    )
+}
