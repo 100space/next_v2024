c489d914dba3190a37a6168315fe6f34445c420a
BAEK'space <112856532+100space@users.noreply.github.com>
2024-09-20 08:29:00 +0900

    18강

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,8 @@ import BookItem from "@/components/book-item"
 
 /**
  * ssr을 위한 함수
- * next.js page route에서는 getServerSideProps 함수를 export 하는 경우 ssr이 적용된다.
- * 서버측에서만 동작하는 함수로 해당 함수 안에서 console.log를 찍어보면 서버측에서만 찍히는 것을 확인할 수 있다.
+ * - next.js page route에서는 getServerSideProps 함수를 export 하는 경우 ssr이 적용된다.
+ * - 서버측에서만 동작하는 함수로 해당 함수 안에서 console.log를 찍어보면 서버측에서만 찍히는 것을 확인할 수 있다.
  */
 export const getServerSideProps = () => {
     // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터를 불러오는 함수
